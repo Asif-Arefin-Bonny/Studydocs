bde17ebddedd5eef52f8adf73e22590206696c5e
Gulshan Jubaed Prince <prince@techynaf.com>
2019-02-18 17:41:41 +0600

    Integrated home page & index page with card partials

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,39 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<!-- Page Features -->
+<div class="row text-center mt-4">
+    <!-- TODO:: need to put the button and dropdown under one form and keep style same -->
+    <div class="col-lg-6 col-md-6">
+      <div class="input-group ">http://127.0.0.1:8000/home
+        <select class="custom-select" id="inputGroupSelect01">
+          <option selected>Choose Subject</option>
+          <option value="1">Web</option>
+          <option value="2">Cloud</option>
+          <option value="3">Data Structure</option>
+        </select>
+      </div>
     </div>
+    <div class="col-lg-6 col-md-6">
+      <form class="form-inline">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search Docs"
+          aria-label="Search"
+        />
+        <button
+          class="btn btn-outline-warning my-2 my-sm-0 text-dark"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  </div>
+  <div class="row text-center mt-4">
+    @include('partials.card')
+  </div>
+      <!-- /.row -->
 </div>
 @endsection
---
+++ resources/views/partials/card.blade.php
@@ -0,0 +1,12 @@
+<div class="col-lg-3 col-md-6 mb-4">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+            Some quick example text to build on the card title and make up
+            the bulk of the card's content.
+            </p>
+            <a href="single.html" class="card-link">Details>></a>
+        </div>
+    </div>
+</div>
