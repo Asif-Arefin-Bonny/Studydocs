eb3a00572f4c7187dca5e8dbe5e6f75fec4fc120
Gulshan Jubaed Prince <prince@techynaf.com>
2019-02-20 02:05:24 +0600

    functionalities done

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'NotesController@showByCategory')->name('home');
 
 // Notes Routes
-Route::get('/notes', 'NotesController@showAll')->name('show-notes');
+Route::get('/', 'NotesController@showAll')->name('show-notes');
 Route::get('/notes/{id}', 'NotesController@show')->name('show-note');
 Route::get('/note/download/{id}', 'DownloadNotesController@downloadNote')->name('download-note');
 Route::get('/note/upload', 'UploadNotesController@create')->name('create-note')->middleware('verified');
